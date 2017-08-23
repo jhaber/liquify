39f1740ca0fcc4e582c2bf8e20494bcb16ba9691
Jonathan Haber <jhaber@hubspot.com>
2017-08-23 10:42:33 -0400

    Make this work for liquibase 2.0.5

--- src/main/java/com/refactify/Liquify.java
+++ src/main/java/com/refactify/Liquify.java
@@ -1,13 +1,32 @@
 package com.refactify;
 
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import com.refactify.arguments.ConversionArguments;
 import com.refactify.arguments.ConversionArgumentsParser;
 import com.refactify.arguments.TargetFileNameBuilder;
+import com.refactify.printer.SqlFormattingWriter;
 import com.refactify.printer.UsagePrinter;
+
+import liquibase.change.Change;
 import liquibase.changelog.ChangeLogParameters;
 import liquibase.changelog.ChangeSet;
 import liquibase.changelog.DatabaseChangeLog;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.core.MySQLDatabase;
 import liquibase.exception.LiquibaseException;
+import liquibase.exception.MigrationFailedException;
+import liquibase.exception.SetupException;
+import liquibase.executor.Executor;
+import liquibase.executor.ExecutorService;
+import liquibase.executor.LoggingExecutor;
 import liquibase.parser.ChangeLogParser;
 import liquibase.parser.ChangeLogParserFactory;
 import liquibase.resource.FileSystemResourceAccessor;
@@ -15,23 +34,58 @@ import liquibase.resource.ResourceAccessor;
 import liquibase.serializer.ChangeLogSerializer;
 import liquibase.serializer.ChangeLogSerializerFactory;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 public class Liquify {
     private final static ConversionArgumentsParser parser = new ConversionArgumentsParser();
     private final static UsagePrinter usagePrinter = new UsagePrinter();
     private final static TargetFileNameBuilder targetFileNameBuilder = new TargetFileNameBuilder();
 
     public static void main(final String[] args) {
+        //ChangeLogSerializerFactory.getInstance().register(new FormattedSqlChangeLogSerializer());
+
         ConversionArguments conversionArguments = parser.parseArguments(args);
         if(conversionArguments.areValid()) {
-            convertDatabaseChangeLog(conversionArguments);
+            String targetFileName = targetFileNameBuilder.buildFilename(conversionArguments);
+
+            try (Writer writer = new FileWriter(targetFileName)) {
+                Executor realExecutor = ExecutorService.getInstance().getExecutor(mysql());
+                Executor loggingExecutor = new LoggingExecutor(realExecutor, new SqlFormattingWriter(writer), mysql());
+                ExecutorService.getInstance().setExecutor(mysql(), loggingExecutor);
+
+                executeChangeLog(conversionArguments, writer);
+            } catch (Exception e) {
+                deleteTargetFile(targetFileName);
+                throw new RuntimeException(e);
+            }
         }
         else {
             usagePrinter.printUsage();
+        }
+    }
+
+    private static void executeChangeLog(final ConversionArguments conversionArguments, Writer writer) throws Exception {
+        ResourceAccessor resourceAccessor = new FileSystemResourceAccessor(System.getProperty("user.dir"));
+        ChangeLogParser parser = ChangeLogParserFactory.getInstance().getParser(conversionArguments.getSource(), resourceAccessor);
+        DatabaseChangeLog changeLog = parser.parse(conversionArguments.getSource(), new ChangeLogParameters(), resourceAccessor);
+
+        writer.write("--liquibase formatted sql\n\n");
+
+        for (ChangeSet changeSet : changeLog.getChangeSets()) {
+            String author = changeSet.getAuthor()
+                    .replaceAll("\\s+", "_")
+                    .replace("_(generated)","");
+
+            writer.write("--changeset " + author + ":" + changeSet.getId() + "\n");
+            for (Change change : changeSet.getChanges()) {
+                try {
+                    change.init();
+                } catch (SetupException se) {
+                    throw new MigrationFailedException(changeSet, se);
+                }
+            }
+
+            for (Change change : changeSet.getChanges()) {
+                mysql().executeStatements(change, changeLog, changeSet.getSqlVisitors());
+            }
         }
     }
 
@@ -43,7 +97,7 @@ public class Liquify {
             DatabaseChangeLog changeLog = parser.parse(conversionArguments.getSource(), new ChangeLogParameters(), resourceAccessor);
             ChangeLogSerializer serializer = ChangeLogSerializerFactory.getInstance().getSerializer(targetFileName);
             for (ChangeSet set : changeLog.getChangeSets()) {
-                set.setFilePath(targetFileName);
+                setFilePath(set, targetFileName);
             }
             serializer.write(changeLog.getChangeSets(), new FileOutputStream(targetFileName));
         }
@@ -69,5 +123,25 @@ public class Liquify {
         catch (IOException ioe) {
             ioe.printStackTrace();
         }
+    }
+
+    private static void setFilePath(ChangeSet changeSet, String filePath) {
+        try {
+            Field f = ChangeSet.class.getDeclaredField("filePath");
+            f.setAccessible(true);
+            f.set(changeSet, filePath);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Database mysql() {
+        for (Database database : DatabaseFactory.getInstance().getImplementedDatabases()) {
+            if (database instanceof MySQLDatabase) {
+                return database;
+            }
+        }
+
+        throw new RuntimeException();
     }
 }
---
+++ src/main/java/com/refactify/printer/SqlFormattingWriter.java
@@ -0,0 +1,116 @@
+package com.refactify.printer;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SqlFormattingWriter extends Writer {
+  private static final String WHITESPACE = " \n\r\f\t";
+
+  private final Writer delegate;
+  private final StringBuilder buffer;
+
+  public SqlFormattingWriter(Writer delegate) {
+    this.delegate = delegate;
+    this.buffer = new StringBuilder();
+  }
+
+  @Override
+  public void write(char[] cbuf, int off, int len) throws IOException {
+    for (int i = off; i < off + len; i++) {
+      char c = cbuf[i];
+
+      if (buffer.length() == 0 && WHITESPACE.indexOf(c) > -1) {
+        delegate.write(c);
+      } else {
+        buffer.append(c);
+        if (c == ';') {
+          flushSql();
+        }
+      }
+
+    }
+  }
+
+  @Override
+  public void flush() throws IOException {
+    delegate.flush();
+  }
+
+  @Override
+  public void close() throws IOException {
+    if (buffer.length() > 0) {
+      delegate.write(buffer.toString());
+      buffer.setLength(0);
+    }
+    delegate.close();
+  }
+
+  private void flushSql() throws IOException {
+    String unformatted = buffer.toString();
+    String formatted = formatSql(unformatted);
+
+    delegate.write(formatted);
+    buffer.setLength(0);
+  }
+
+  private String formatSql(String unformatted) {
+    if (!unformatted.startsWith("CREATE TABLE")) {
+      return unformatted;
+    }
+
+    StringBuilder formatted = new StringBuilder();
+    int openParen = unformatted.indexOf('(');
+    formatted.append(unformatted.substring(0, openParen + 1));
+
+    int closeParen = unformatted.lastIndexOf(')');
+
+    String tableDef = unformatted.substring(openParen + 1, closeParen);
+
+    formatted.append('\n');
+    for (String line : splitLines(tableDef)) {
+      formatted.append("  ").append(line).append("\n");
+    }
+
+    formatted.append(')');
+    // force a space after the close paren
+    if (unformatted.charAt(closeParen + 1) != ' ') {
+      formatted.append(' ');
+    }
+    formatted.append(unformatted.substring(closeParen + 1));
+
+    return formatted.toString();
+  }
+
+  // don't split enum defs onto separate lines
+  private List<String> splitLines(String tableDef) {
+    List<String> lines = new ArrayList<>();
+    StringBuilder currentLine = new StringBuilder();
+    int openParens = 0;
+
+    for (char c : tableDef.toCharArray()) {
+      if (currentLine.length() == 0 && WHITESPACE.indexOf(c) > -1) {
+        continue;
+      }
+
+      currentLine.append(c);
+
+      if (c == '(') {
+        openParens++;
+      } else if (c == ')') {
+        openParens--;
+      } else if (c == ',' && openParens == 0) {
+        lines.add(currentLine.toString());
+        currentLine = new StringBuilder();
+      }
+    }
+
+    if (currentLine.length() > 0) {
+      lines.add(currentLine.toString());
+      currentLine = null;
+    }
+
+    return lines;
+  }
+}
